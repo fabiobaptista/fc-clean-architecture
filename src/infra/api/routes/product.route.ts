70ad089059c8cdbf2f7ff0751d5a744cd6d35a0a
fabiobaptista <fhbaptista@gmail.com>
2022-05-26 17:43:02 -0300

    feat: ensure list all products

--- src/infra/api/routes/product.route.ts
+++ src/infra/api/routes/product.route.ts
@@ -1,9 +1,22 @@
 import ProductRepo from '@/infra/product/repo/sequelize/product.repo'
 import { InputCreateProductDto, OutputCreateProductDto } from '@/usescases/product/create/create-product.dto'
 import CreateProductUseCase from '@/usescases/product/create/create-product.usecase'
+import { OutputListProductsDto } from '@/usescases/product/list/list-product.dto'
+import { ListProductUseCase } from '@/usescases/product/list/list-product.usecase'
 import express, { Router, Request, Response } from 'express'
 
 export const productRoute: Router = express.Router()
+
+productRoute.get('/', async (req: Request, res: Response): Promise<void> => {
+  const usecase = new ListProductUseCase(new ProductRepo())
+  try {
+    const output: OutputListProductsDto = await usecase.execute({})
+
+    res.status(200).send(output)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
 
 productRoute.post('/', async (req: Request, res: Response): Promise<void> => {
   try {
@@ -23,24 +36,3 @@ productRoute.post('/', async (req: Request, res: Response): Promise<void> => {
     res.status(500).send(error)
   }
 })
-
-// customerRoute.post('/', async (req: Request, res: Response): Promise<void> => {
-//   const usecase = new CreateCustomerUseCase(new CustomerRepo())
-//   try {
-//     const input: InputCreateCustomerDto = {
-//       name: req.body.name,
-//       address: {
-//         street: req.body.address.street,
-//         number: req.body.address.number,
-//         city: req.body.address.city,
-//         zip: req.body.address.zip
-//       }
-//     }
-
-//     const output: OutputCreateCustomerDto = await usecase.execute(input)
-
-//     res.status(200).send(output)
-//   } catch (error) {
-//     res.status(500).send(error)
-//   }
-// })
