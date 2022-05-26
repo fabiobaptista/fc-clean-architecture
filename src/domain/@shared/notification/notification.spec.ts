d30a15a58dbbaa0fe083c5bb32c72b4f84048884
fabiobaptista <fhbaptista@gmail.com>
2022-05-26 20:23:12 -0300

    test: ensure create and filter notification messages

---
+++ src/domain/@shared/notification/notification.spec.ts
@@ -0,0 +1,78 @@
+import NotificationError from './notification-error'
+
+describe('Unit test for Notification', () => {
+  test('should create a error', () => {
+    const notification = new NotificationError()
+
+    const error = {
+      message: 'error message',
+      context: 'customer'
+    }
+    const formatedMessage = `${error.context}: ${error.message}`
+
+    notification.addError(error)
+
+    const messages = notification.messages('customer')
+
+    expect(messages.split(',').length).toBe(1)
+    expect(messages).toBe(formatedMessage)
+  })
+
+  test('should create many errors', () => {
+    const notification = new NotificationError()
+
+    const error = {
+      message: 'error message',
+      context: 'customer'
+    }
+    const formatedMessage = `${error.context}: ${error.message}`
+
+    notification.addError(error)
+    notification.addError(error)
+
+    expect(notification.messages('customer')).toBe(`${formatedMessage},${formatedMessage}`)
+  })
+
+  test('should returns only messages of customer context', () => {
+    const notification = new NotificationError()
+
+    const error1 = {
+      message: 'error message',
+      context: 'customer'
+    }
+    const formatedMessage1 = `${error1.context}: ${error1.message}`
+
+    notification.addError(error1)
+
+    const error2 = {
+      message: 'error message',
+      context: 'order'
+    }
+
+    notification.addError(error2)
+
+    expect(notification.messages('customer')).toBe(`${formatedMessage1}`)
+  })
+
+  test('should returns all messages', () => {
+    const notification = new NotificationError()
+
+    const error1 = {
+      message: 'error message',
+      context: 'customer'
+    }
+    const formatedMessage1 = `${error1.context}: ${error1.message}`
+
+    notification.addError(error1)
+
+    const error2 = {
+      message: 'error message',
+      context: 'order'
+    }
+    const formatedMessage2 = `${error2.context}: ${error2.message}`
+
+    notification.addError(error2)
+
+    expect(notification.messages()).toBe(`${formatedMessage1},${formatedMessage2}`)
+  })
+})
