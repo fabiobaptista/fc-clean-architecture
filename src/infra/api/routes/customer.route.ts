168cbe52044a643bbdb28017d4caec35309632d5
fabiobaptista <fhbaptista@gmail.com>
2022-05-26 17:22:58 -0300

    feat: ensure list all customers

--- src/infra/api/routes/customer.route.ts
+++ src/infra/api/routes/customer.route.ts
@@ -1,9 +1,22 @@
-import CustomerRepo from '@/infra/customer/repo/sequelize/customer.repo'
-import { InputCreateCustomerDto, OutputCreateCustomerDto } from '@/usescases/customer/create/create-customer.dto'
-import CreateCustomerUseCase from '@/usescases/customer/create/create-customer.usecase'
 import express, { Router, Request, Response } from 'express'
+import CustomerRepo from '@/infra/customer/repo/sequelize/customer.repo'
+import CreateCustomerUseCase from '@/usescases/customer/create/create-customer.usecase'
+import ListCustomerUseCase from '@/usescases/customer/list/list-customer.usecase'
+import { InputCreateCustomerDto, OutputCreateCustomerDto } from '@/usescases/customer/create/create-customer.dto'
+import { OutputListCustomersDto } from '@/usescases/customer/list/list-customer.dto'
 
 export const customerRoute: Router = express.Router()
+
+customerRoute.get('/', async (req: Request, res: Response): Promise<void> => {
+  const usecase = new ListCustomerUseCase(new CustomerRepo())
+  try {
+    const output: OutputListCustomersDto = await usecase.execute({})
+
+    res.status(200).send(output)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
 
 customerRoute.post('/', async (req: Request, res: Response): Promise<void> => {
   const usecase = new CreateCustomerUseCase(new CustomerRepo())
@@ -20,12 +33,8 @@ customerRoute.post('/', async (req: Request, res: Response): Promise<void> => {
 
     const output: OutputCreateCustomerDto = await usecase.execute(input)
 
-    res
-      .status(200)
-      .send(output)
+    res.status(200).send(output)
   } catch (error) {
-    res
-      .status(500)
-      .send(error)
+    res.status(500).send(error)
   }
 })
