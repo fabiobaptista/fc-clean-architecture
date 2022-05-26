da578a95e4eaaf3101b912f14d07fd20638d1f77
fabiobaptista <fhbaptista@gmail.com>
2022-05-26 17:40:19 -0300

    test: ensure create a product and not create a product

---
+++ src/infra/api/__tests__/product.e2e.spec.ts
@@ -0,0 +1,84 @@
+import { app, sequelize } from '../express'
+import request from 'supertest'
+import { InputCreateProductDto } from '@/usescases/product/create/create-product.dto'
+import { OutputListProductsDto } from '@/usescases/product/list/list-product.dto'
+
+describe('E2E test to Product', () => {
+  beforeEach(async () => {
+    await sequelize.sync({ force: true })
+  })
+
+  afterAll(async () => {
+    await sequelize.close()
+  })
+
+  test('should create a product', async () => {
+    const input: InputCreateProductDto = {
+      id: '1',
+      name: 'Product',
+      price: 100
+    }
+    const { status, body }: {
+      status: number
+      body: InputCreateProductDto
+    } = await request(app).post('/product').send(input)
+
+    expect(status).toBe(200)
+    expect(body.id).toBe(input.id)
+    expect(body.name).toBe(input.name)
+    expect(body.price).toBe(input.price)
+  })
+
+  test('should not create a product', async () => {
+    const input: InputCreateProductDto = {
+      id: '',
+      name: 'Product',
+      price: 100
+    }
+    const { status }: {
+      status: number
+    } = await request(app).post('/product').send(input)
+
+    expect(status).toBe(500)
+  })
+
+  test('should list all customers', async () => {
+    {
+      const input: InputCreateProductDto = {
+        id: '1',
+        name: 'Product',
+        price: 100
+      }
+      const { status, body }: {
+        status: number
+        body: InputCreateProductDto
+      } = await request(app).post('/product').send(input)
+
+      expect(status).toBe(200)
+      expect(body.id).toBe(input.id)
+    }
+
+    {
+      const input: InputCreateProductDto = {
+        id: '2',
+        name: 'Product',
+        price: 100
+      }
+      const { status, body }: {
+        status: number
+        body: InputCreateProductDto
+      } = await request(app).post('/product').send(input)
+
+      expect(status).toBe(200)
+      expect(body.id).toBe(input.id)
+    }
+
+    const { status, body }: {
+      status: number
+      body: OutputListProductsDto
+    } = await request(app).get('/customer').send()
+
+    expect(status).toBe(200)
+    expect(body.products.length).toBe(2)
+  })
+})
